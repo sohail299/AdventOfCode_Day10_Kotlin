9b229e5de60a01c8208052ac284b9c77fdb4af50
sohail299 <muhammadsohail299@gmail.com>
2022-10-30 02:03:29 +0200

    Input File read and split values

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
+import java.io.FileNotFoundException
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    //Read input file
+    try {
+        var input = File("src/main/kotlin/input.txt").readLines()
+        println(splitInput(input))
+    }catch (e: FileNotFoundException){
+        println("Input File not found")
+    }
+
+}
+fun splitInput(input: List<String>): List<String> {
+    val list : ArrayList<String> = ArrayList()
+
+    input.toString().split(",", "<", ">").map { it.trim() }.run {
+        for (value in 0 until (input.toString()).length){
+            list.add(this[1])
+            list.add(this[2])
+            list.add(this[4])
+            list.add(this[5])
+            }
+
+        return list
+    }
 }
---
+++ src/main/kotlin/StarPositionAndVelocity.kt
@@ -0,0 +1,5 @@
+data class StarPositionAndVelocity(
+    var positionX: Int, var positionY: Int,
+    var velocityX: Int, var velocityY: Int,
+    var time: Int = 0
+)
