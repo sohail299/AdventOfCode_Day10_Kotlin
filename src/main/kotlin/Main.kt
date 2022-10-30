a160c20c65bc78370b5f2010136ada365080d1f1
sohail299 <muhammadsohail299@gmail.com>
2022-10-30 03:12:05 +0100

    calculate minimum distance among stars by inputting the actual advent od code input.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,33 +6,39 @@ fun main(args: Array<String>) {
     //Read input file
     try {
         val input = File("src/main/kotlin/input.txt").readLines().map { splitInput(it) }
-        minimum(input)
-
+        //   minimum(input)
+        val minimum = (0..1000).map { t -> input.map { it.movement(t) } }
+            .minBy { d -> closestDistance(d) }
+        println("t=${minimum.first().time}")
     } catch (e: FileNotFoundException) {
         println("Input File not found")
     }
 }
 
-//Get the  minimum time and map the data
-fun minimum(input: List<StarPositionAndVelocity>){
-    for(i in input.indices){
-       var t = input.map { it.movement(i) }
-
-        println("${t[i].positionX}     ${t[i].positionY}   ${t[i].time}")
-
-    }
+//Get the closest minimum distance Time among stars
+fun closestDistance(star: List<StarPositionAndVelocity>): Int {
+    var minDistance = 0
+    val maxX = star.map { it.positionX }.max()
+    val minX = star.map { it.positionX }.min()
+    val maxY = star.map { it.positionY }.max()
+    val minY = star.map { it.positionY }.min()
+    minDistance = maxX - minX + maxY - minY
+    return minDistance
 }
+
 //split input values
 fun splitInput(input: String): StarPositionAndVelocity {
     val list: ArrayList<String> = ArrayList()
     input.split(",", "<", ">").map { it.trim() }.run {
-            list.add(this[1])
-            list.add(this[2])
-            list.add(this[4])
-            list.add(this[5])
-        return StarPositionAndVelocity((this[1].toInt()),
-                (this[2].toInt()),
-                (this[4].toInt()),
-              (this[5].toInt()))
+        list.add(this[1])
+        list.add(this[2])
+        list.add(this[4])
+        list.add(this[5])
+        return StarPositionAndVelocity(
+            (this[1].toInt()),
+            (this[2].toInt()),
+            (this[4].toInt()),
+            (this[5].toInt())
+        )
     }
 }
