d0524be38781ceaf2430fa60e7a17e40aa2f6352
sohail299 <muhammadsohail299@gmail.com>
2022-10-30 02:46:45 +0100

    logic implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,24 +5,34 @@ fun main(args: Array<String>) {
 
     //Read input file
     try {
-        var input = File("src/main/kotlin/input.txt").readLines()
-        println(splitInput(input))
-    }catch (e: FileNotFoundException){
+        val input = File("src/main/kotlin/input.txt").readLines().map { splitInput(it) }
+        minimum(input)
+
+    } catch (e: FileNotFoundException) {
         println("Input File not found")
     }
-
 }
-fun splitInput(input: List<String>): List<String> {
-    val list : ArrayList<String> = ArrayList()
 
-    input.toString().split(",", "<", ">").map { it.trim() }.run {
-        for (value in 0 until (input.toString()).length){
+//Get the  minimum time and map the data
+fun minimum(input: List<StarPositionAndVelocity>){
+    for(i in input.indices){
+       var t = input.map { it.movement(i) }
+
+        println("${t[i].posX}     ${t[i].posY}   ${t[i].t}")
+
+    }
+}
+//split input values
+fun splitInput(input: String): StarPositionAndVelocity {
+    val list: ArrayList<String> = ArrayList()
+    input.split(",", "<", ">").map { it.trim() }.run {
             list.add(this[1])
             list.add(this[2])
             list.add(this[4])
             list.add(this[5])
-            }
-
-        return list
+        return StarPositionAndVelocity((this[1].toInt()),
+                (this[2].toInt()),
+                (this[4].toInt()),
+              (this[5].toInt()))
     }
 }
--- src/main/kotlin/StarPositionAndVelocity.kt
+++ src/main/kotlin/StarPositionAndVelocity.kt
@@ -2,4 +2,18 @@ data class StarPositionAndVelocity(
     var positionX: Int, var positionY: Int,
     var velocityX: Int, var velocityY: Int,
     var time: Int = 0
-)
+) {
+    var posX = 0
+    var posY = 0
+    var t = 0
+
+    //movement of starts
+    fun movement(t: Int) : StarPositionAndVelocity {
+
+        posX = positionX * velocityX + time
+        posY = positionY * velocityY + time
+        this.t = t + time
+        return StarPositionAndVelocity(posX, posY, velocityX, velocityY, this.t)
+    }
+
+}
